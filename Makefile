35063a0385d24590f21ccb04c367743071cc5be7
jelgar <jamesnelgar@gmail.com>
2020-11-26 09:48:59 +0000

    Added vagrant image and drew a rectangle

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 default: build 
 
-build: main.cpp
-	g++ $? -o $@ -Wl,-rpath,/usr/local/lib `pkg-config --cflags --libs opencv4`
+build: faces/face.cpp
+	g++ faces/face.cpp -o faces.o /usr/lib64/libopencv_core.so /usr/lib64/libopencv_highgui.so.2.4 /usr/lib64/libopencv_imgproc.so.2.4 /usr/lib64/libopencv_objdetect.so.2.4
--- faces/face.cpp
+++ faces/face.cpp
@@ -17,6 +17,24 @@
 using namespace std;
 using namespace cv;
 
+class Rectangle {
+  public:
+    Point v0; 
+    Point v1; 
+    Rectangle(Point v0, Point v1);
+    void draw(Mat frame);
+};
+
+Rectangle::Rectangle(Point v0, Point v1) {
+  this->v0 = v0;
+  this->v1 = v1;
+}
+
+void Rectangle::draw(Mat frame) {
+    rectangle(frame, v0, v1, Scalar( 0, 0, 255 ), 2);
+}
+
+
 /** Function Headers */
 void detectAndDisplay( Mat frame );
 
@@ -28,14 +46,24 @@ CascadeClassifier cascade;
 /** @function main */
 int main( int argc, const char** argv )
 {
-       // 1. Read Input Image
-	Mat frame = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    // 0. Setup true faces
+    std::vector<Rectangle> true_faces; 
+    true_faces.push_back(Rectangle(Point(327, 96), Point(492, 278)));
+
+    // 1. Read Input Image
+	Mat frame = imread(argv[1], IMREAD_COLOR);
 
 	// 2. Load the Strong Classifier in a structure called `Cascade'
 	if( !cascade.load( cascade_name ) ){ printf("--(!)Error loading\n"); return -1; };
 
 	// 3. Detect Faces and Display Result
 	detectAndDisplay( frame );
+
+    // 3.5 Draw true faces
+	for( int i = 0; i < true_faces.size(); i++ )
+	{
+      true_faces[i].draw(frame);
+    }
 
 	// 4. Save Result Image
 	imwrite( "detected.jpg", frame );
@@ -50,11 +78,11 @@ void detectAndDisplay( Mat frame )
 	Mat frame_gray;
 
 	// 1. Prepare Image by turning it into Grayscale and normalising lighting
-	cvtColor( frame, frame_gray, CV_BGR2GRAY );
+	cvtColor( frame, frame_gray, COLOR_BGR2GRAY );
 	equalizeHist( frame_gray, frame_gray );
 
 	// 2. Perform Viola-Jones Object Detection 
-	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CV_HAAR_SCALE_IMAGE, Size(50, 50), Size(500,500) );
+	cascade.detectMultiScale( frame_gray, faces, 1.1, 1, 0|CASCADE_SCALE_IMAGE, Size(50, 50), Size(500,500) );
 
        // 3. Print number of Faces found
 	std::cout << faces.size() << std::endl;
